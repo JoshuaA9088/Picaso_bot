9c2b6eb49c656b57859d535b31037d00a27fb35e
JoshuaA9088 <joshua.gf.arul@gmail.com>
2019-08-13 20:41:06 -0700

    V2.0 Now supports color!

--- Picaso.py
+++ Picaso.py
@@ -1,5 +1,4 @@
-# 2019_7_31
-# Picaso V1.1 Work in progress.
+# Picaso V2.0 Work in progress.
 # - Install OpenCV, pynput, and pillow (pip install opencv-python pynput pillow).
 # - Do not touch or move the mouse while running.
 # - To terminate, press ESC.
@@ -9,14 +8,13 @@ import os
 import time
 import numpy as np
 from PIL import ImageGrab
+from itertools import combinations
+from collections import Counter
 
 from pynput.keyboard import Key, Controller as KeyboardController
 from pynput.mouse import Button, Controller as MouseController
 
 from pynput.keyboard import Listener
-
-keyboard = KeyboardController()
-mouse = MouseController()
 
 mouse = MouseController()
 keyboard = KeyboardController()
@@ -77,9 +75,9 @@ def find_fields():
     bottom_right = (top_left[0] + w, top_left[1] + h)
 
     # Calculate coordinates of fields, assuming constant distance between boxes.
-    red = ((top_left[0] + bottom_right[0]) / 2, (top_left[1] + bottom_right[1]) / 2)
-    blue = (red[0], red[1] + 20)
-    green = (red[0], red[1] + 40)
+    red = ((top_left[0] + bottom_right[0]) / 2 + 25, (top_left[1] + bottom_right[1]) / 2)
+    blue = (red[0], red[1] + 25)
+    green = (red[0], red[1] + 50)
 
     # Match to template OK button.
     res = cv2.matchTemplate(img, ok_button, cv2.TM_SQDIFF)
@@ -106,7 +104,8 @@ def clear_field():
         keyboard.press(Key.delete)
 
 
-def update_RGB(grayThresh):
+def update_RGB(r, g, b):
+    time.sleep(1)
     # SELECT COLOR Button pixel coordinates
     mouse.position = find_edit_button()
     mouse.click(Button.left, 1)
@@ -118,21 +117,21 @@ def update_RGB(grayThresh):
     mouse.click(Button.left, 1)
     clear_field()
     # Enter the max rgb value (grayThresh) in COLOR\RED
-    keyboard.type(str(grayThresh))
+    keyboard.type(str(r))
 
     # SELECT COLOR\BLUE Button pixel coordinates
     mouse.position = fields["blue"]
     mouse.click(Button.left, 1)
     clear_field()
     # Enter the max rgb value (grayThresh) in COLOR\BLUE
-    keyboard.type(str(grayThresh))
+    keyboard.type(str(g))
 
     # SELECT COLOR\GREEN Button pixel coordinates
     mouse.position = fields["green"]
     mouse.click(Button.left, 1)
     clear_field()
     # Enter the max rgb value (grayThresh) in COLOR\GREEN
-    keyboard.type(str(grayThresh))
+    keyboard.type(str(b))
 
     time.sleep(0.1)
     # OK Button pixel coordinates
@@ -141,46 +140,44 @@ def update_RGB(grayThresh):
     mouse.click(Button.left, 1)
     # Reseting mouse position to starting position
     mouse.position = START_POS
-    time.sleep(0.1)
+    time.sleep(0.5)
 
 
 def draw_image(imgPath):
     img = cv2.imread(imgPath)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # Gray brightness value AKA current max rgb value
-    grayThresh = 0
-    # Gray brightness value increase amount
-    # Tweak for speed
-    threshStep = 1
+    height, width, channels = img.shape
 
-    while grayThresh < 255:
-        # Prints the current max rgb value
-        print("GrayThresh :", grayThresh)
-        # Prints the percentage (%)
-        print(str(int((grayThresh / 255) * 100)) + "%")
+    # Ignore color if its less than X percent of image.
+    pixel_tolerance = int((height * width) * .001)
+    all_rgb_codes = img.reshape(-1, img.shape[-1])
+    unique_rgbs, counts = np.unique(all_rgb_codes, axis=0, return_counts=True)
 
-        # Skip values that aren't present in the image
-        if grayThresh in img:
-            print("GrayThresh: " + str(grayThresh))
-            update_RGB(grayThresh)
-        else:
-            grayThresh += threshStep
+    # print("Total Number of RGB Iterations: {}".format(len(unique_rgbs)))
+    # print("Pixel Tolerance: {}".format(pixel_tolerance))
+    for ((r, g, b), c) in zip(unique_rgbs, counts):
+        if c < pixel_tolerance:
             continue
+        elif r == 255 and g == 255 and b == 255:
+            continue
+
+        print("R: {0}, G: {1}, B: {2}".format(r, g, b))
+        # print("Number of Pixels: {}".format(num_pixels))
+        update_RGB(r, g, b)
 
         for i in range(len(img)):
             mouse.position = (START_POS[0], mouse.position[1])
             mouse.move(0, 1)
             for j in range(len(img[i])):
-                if grayThresh - threshStep < img[i, j, 0] and img[i, j, 0] <= grayThresh:
+                if list(img[i, j]) == [r, g, b]:
                     # Click (paint)
                     mouse.click(Button.left, 1)
                     # Speed, faster more errors
-                    time.sleep(0.0005)
+                    time.sleep(0.00001)
                 mouse.move(1, 0)
-        grayThresh += threshStep
-
 
 # Keyboard termination, ESC
 listener = Listener(on_press=on_press)
 listener.start()
-draw_image("Images/Wizard.png")
+time.sleep(10)
+draw_image("Images/mario.png")
